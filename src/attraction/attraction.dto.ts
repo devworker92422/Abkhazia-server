c414a90ef8cc199024848f2dfd22adcde4ce2659
dounine423 <dounine423@gmail.com>
2024-06-27 21:33:55 +0900

    add the active field on update function

--- src/attraction/attraction.dto.ts
+++ src/attraction/attraction.dto.ts
@@ -23,6 +23,7 @@ export interface UpdateAttractionBodyDTO {
         longitude?: number;
         heading?: string;
         directionID?: number;
+        active?: boolean;
     },
     direction?: {
         id: number;
